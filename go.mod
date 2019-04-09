e72846e0643324ad6686157674731c267b608ce5
Derrick J. Wippler <thrawn01@gmail.com>
2019-04-09 17:36:47 -0500

    Added support for go modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/golang/groupcache
+
+require github.com/golang/protobuf v1.3.1
---
+++ proto.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Make sure the script fails fast.
+set -e
+set -u
+set -x
+
+PROTO_DIR=groupcachepb
+
+protoc -I=$PROTO_DIR \
+    --go_out=$PROTO_DIR \
+    $PROTO_DIR/groupcache.proto
