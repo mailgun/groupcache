bde425012912a72d97ba7f11163db3932f6621df
Derrick J. Wippler <thrawn01@gmail.com>
2022-12-20 11:35:12 -0600

    Fix LRU to correctly update expire on value replace (#56)

--- go.mod
+++ go.mod
@@ -1,10 +1,19 @@
 module github.com/mailgun/groupcache/v2
 
+go 1.19
+
 require (
-	github.com/golang/protobuf v1.3.1
+	github.com/golang/protobuf v1.5.2
 	github.com/segmentio/fasthash v1.0.3
-	github.com/sirupsen/logrus v1.6.0
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	github.com/sirupsen/logrus v1.9.0
+	github.com/stretchr/testify v1.8.1
 )
 
-go 1.15
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- groupcache_test.go
+++ groupcache_test.go
@@ -30,9 +30,9 @@ import (
 	"unsafe"
 
 	"github.com/golang/protobuf/proto"
-
 	pb "github.com/mailgun/groupcache/v2/groupcachepb"
 	"github.com/mailgun/groupcache/v2/testpb"
+	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -536,17 +536,105 @@ func TestContextDeadlineOnPeer(t *testing.T) {
 	}
 }
 
-func TestCacheRemovesBytesOfReplacedValues(t *testing.T) {
-	c := cache{}
+func TestEnsureSizeReportedCorrectly(t *testing.T) {
+	c := &cache{}
 
-	const key = "test"
-	keyLen := len(key)
+	// Add the first value
+	bv1 := ByteView{s: "first", e: time.Now().Add(100 * time.Second)}
+	c.add("key1", bv1)
+	v, ok := c.get("key1")
 
-	for _, bytes := range []int{100, 1000, 2000} {
-		c.add(key, ByteView{b: make([]byte, bytes)})
-		expected := int64(bytes + keyLen)
-		if c.nbytes != expected {
-			t.Fatalf("%s: expected %d was %d", t.Name(), expected, c.nbytes)
-		}
+	// Should be len("key1" + "first") == 9
+	assert.True(t, ok)
+	assert.True(t, v.Equal(bv1))
+	assert.Equal(t, int64(9), c.bytes())
+
+	// Add a second value
+	bv2 := ByteView{s: "second", e: time.Now().Add(200 * time.Second)}
+
+	c.add("key2", bv2)
+	v, ok = c.get("key2")
+
+	// Should be len("key2" + "second") == (10 + 9) == 19
+	assert.True(t, ok)
+	assert.True(t, v.Equal(bv2))
+	assert.Equal(t, int64(19), c.bytes())
+
+	// Replace the first value with a shorter value
+	bv3 := ByteView{s: "3", e: time.Now().Add(200 * time.Second)}
+
+	c.add("key1", bv3)
+	v, ok = c.get("key1")
+
+	// len("key1" + "3") == 5
+	// len("key2" + "second") == 10
+	assert.True(t, ok)
+	assert.True(t, v.Equal(bv3))
+	assert.Equal(t, int64(15), c.bytes())
+
+	// Replace the second value with a longer value
+	bv4 := ByteView{s: "this-string-is-28-chars-long", e: time.Now().Add(200 * time.Second)}
+
+	c.add("key2", bv4)
+	v, ok = c.get("key2")
+
+	// len("key1" + "3") == 5
+	// len("key2" + "this-string-is-28-chars-long") == 32
+	assert.True(t, ok)
+	assert.True(t, v.Equal(bv4))
+	assert.Equal(t, int64(37), c.bytes())
+}
+
+func TestEnsureUpdateExpiredValue(t *testing.T) {
+	c := &cache{}
+	curTime := time.Now()
+
+	// Override the now function so we control time
+	NowFunc = func() time.Time {
+		return curTime
 	}
+
+	// Expires in 1 second
+	c.add("key1", ByteView{s: "value1", e: curTime.Add(time.Second)})
+	_, ok := c.get("key1")
+	assert.True(t, ok)
+
+	// Advance 1.1 seconds into the future
+	curTime = curTime.Add(time.Millisecond * 1100)
+
+	// Value should have expired
+	_, ok = c.get("key1")
+	assert.False(t, ok)
+
+	// Add a new key that expires in 1 second
+	c.add("key2", ByteView{s: "value2", e: curTime.Add(time.Second)})
+	_, ok = c.get("key2")
+	assert.True(t, ok)
+
+	// Advance 0.5 seconds into the future
+	curTime = curTime.Add(time.Millisecond * 500)
+
+	// Value should still exist
+	_, ok = c.get("key2")
+	assert.True(t, ok)
+
+	// Replace the existing key, this should update the expired time
+	c.add("key2", ByteView{s: "updated value2", e: curTime.Add(time.Second)})
+	_, ok = c.get("key2")
+	assert.True(t, ok)
+
+	// Advance 0.6 seconds into the future, which puts us past the initial
+	// expired time for key2.
+	curTime = curTime.Add(time.Millisecond * 600)
+
+	// Should still exist
+	_, ok = c.get("key2")
+	assert.True(t, ok)
+
+	// Advance 1.1 seconds into the future
+	curTime = curTime.Add(time.Millisecond * 1100)
+
+	// Should not exist
+	_, ok = c.get("key2")
+	assert.False(t, ok)
 }
